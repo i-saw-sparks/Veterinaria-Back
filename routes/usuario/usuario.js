007a8551ce7835265d022f2272695e43e80fe845
Andres Molina <andresmolinaceti@gmail.com>
2021-11-27 17:59:12 -0600

    POST user protections and pass hashing

--- routes/usuario/usuario.js
+++ routes/usuario/usuario.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const bcrypt = require("bcryptjs");
 
 app.get("/",  (req, res) => {
-    req.app.get("db").query('SELECT * FROM usuarios', (err, rows, fields) =>{
+    req.app.get("db").query('SELECT * FROM usuarios', (err, rows) =>{
         if(err){
             req.app.get("errManager")(res, err.message, "Failed to get users.");
         }else{
@@ -14,19 +15,29 @@ app.get("/",  (req, res) => {
 app.post("/", (req, res) => {
     let data = req.body;
 
-    //Agregar hasheo de contraseña
-
-    req.app.get("db").query("INSERT INTO usuarios " +
-    "(nombre, contrasenia, horario, tipo_usuario, permisos) VALUES" +
-    "('"+data.nombre+"','"+data.contrasenia+"','"+data.horario+"',"+data.tipo_usuario+",'"+data.permisos+"')"
-    , (err, result) =>{
+    req.app.get("db").query("SELECT * FROM usuarios WHERE nombre = '" + data.nombre + "'", (err, rows) =>{
         if(err){
-            req.app.get("errManager")(res, err.message, "Failed to insert user.");
+            req.app.get("errManager")(res, err.message, "Failed to post user."); 
         }else{
-            console.log(result);
-            res.status(200).send();
+            if(rows.length != 0){
+                res.status(400).json({msg:"Este usuario ya existe"});
+            }else{
+                data.contrasenia = bcrypt.hashSync(data.contrasenia, bcrypt.genSaltSync(10));
+
+                req.app.get("db").query("INSERT INTO usuarios " +
+                "(nombre, contrasenia, horario, tipo_usuario, permisos) VALUES" +
+                "('"+data.nombre+"','"+data.contrasenia+"','"+data.horario+"',"+data.tipo_usuario+",'"+data.permisos+"')"
+                , (err, result) =>{
+                    if(err){
+                        req.app.get("errManager")(res, err.message, "Failed to insert user.");
+                    }else{
+                        console.log(result);
+                        res.status(200).json({msg:"Usuario insertado con exito"});
+                    }
+                });
+            }
         }
-    });
+    } )
 
 })
 
