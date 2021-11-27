20a94f6e68ccc5f370b5dea7f656ed2faab2c99e
Andres Molina <andresmolinaceti@gmail.com>
2021-11-27 17:11:32 -0600

    conexion a db

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var cors = require("cors");
 //const keys = require("./keys/keys");
 //const jwt = require("express-jwt");
-const compression = require('compression')
 
-var database;
 var app = express();
-app.use(compression())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : ''
+});
+
+app.use(cors({}))
+
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
-console.log("Loadingdummy data...");
 LOCAL_PORT = 3000;
 
 let manageError = (res, reason, message, code) => {
@@ -27,10 +34,25 @@ let manageError = (res, reason, message, code) => {
 app.set("errManager", manageError);
 require("./startup/mainRouter")(app);
 
-var server = app.listen(process.env.PORT || LOCAL_PORT, function () {
-    var port = server.address().port;
-    console.log("veterinaria-back now running on port ", port);
+console.log("Connecting to the database...");
+connection.connect((err) => {
+    if(err){
+        console.log("Error connecting to the database");
+    }else{
+        console.log("Connected to the database");
+        app.set("db", connection);
+        var server = app.listen(process.env.PORT || LOCAL_PORT, function () {
+            var port = server.address().port;
+            console.log("veterinaria-back now running on port ", port);
+        });
+        
+    }
 });
+
+
+
+
+
 
 
 
