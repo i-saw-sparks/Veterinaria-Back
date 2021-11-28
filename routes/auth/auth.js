e8f858aa9eb43c585c2e31545793958ff6d25a17
Andres Molina <andresmolinaceti@gmail.com>
2021-11-27 18:51:25 -0600

    auth error manager

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -18,6 +18,11 @@ app.post("/", function (req, res) {
     }
 
     req.app.get("db").query("SELECT * FROM usuarios WHERE nombre = '" + data.nombre + "'", (err, rows) => {
+        if(err){
+            req.app.get("errManager")(res, err.message, "Failed to auth, server error.");
+            return
+        }
+
         if (rows.length == 0) {
             res.status(403).json({ msg: "User not found", status: 0 });
             return;
