e8ea8f71e331fe27bce31470c4305564bf15eb9a
Andres Molina <andresmolinaceti@gmail.com>
2021-11-27 22:30:13 -0600

    post all

--- routes/insumo/insumo.js
+++ routes/insumo/insumo.js
@@ -11,4 +11,43 @@ app.get("/", function (req, res) {
     })
 });
 
+app.post("/", (req, res) => {
+    let data = req.body;
+    if (!data.nombre) {
+        res.status(400).json({ msg: "Invalid input, nombre is mandatory" });
+        return;
+    }
+    if (!data.cantidad) {
+        res.status(400).json({ msg: "Invalid input, cantidad is mandatory" });
+        return;
+    }
+    if (!data.precio) {
+        res.status(400).json({ msg: "Invalid input, precio is mandatory" });
+        return;
+    }
+    if (!data.descripcion) {
+        res.status(400).json({ msg: "Invalid input, descripcion is mandatory" });
+        return;
+    }
+    if (!data.tipo) {
+        res.status(400).json({ msg: "Invalid input, tipo is mandatory" });
+        return;
+    }
+    if (!data.id_proveedor) {
+        res.status(400).json({ msg: "Invalid input, id_proveedor is mandatory" });
+        return;
+    }
+
+    req.app.get("db").query("INSERT INTO insumos_medicos " +
+        "(nombre, cantidad, precio, descripcion, tipo, id_proveedor) VALUES" +
+        "('" + data.nombre + "'," + data.cantidad + "," + data.precio + ",'" + data.descripcion + "','" + data.tipo + "'," + data.id_proveedor + ")"
+        , (err, result) => {
+            if (err) {
+                req.app.get("errManager")(res, err.message, "Failed to insert insumo medico.");
+            } else {
+                res.status(200).json({ msg: "Insumo medico insertado con exito" });
+            }
+        });
+})
+
 module.exports = app;
