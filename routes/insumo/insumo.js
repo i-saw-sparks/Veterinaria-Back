bf5f02957c581d43535fd877252b5da5c897bd98
Andres Molina <andresmolinaceti@gmail.com>
2021-11-29 20:37:12 -0600

    put insumo

--- routes/insumo/insumo.js
+++ routes/insumo/insumo.js
@@ -2,34 +2,49 @@ const express = require('express');
 const app = express();
 
 app.get("/", function (req, res) {
-    req.app.get("db").query('SELECT * FROM insumos_medicos', (err, rows) =>{
-        if(err){
+    req.app.get("db").query('SELECT * FROM insumos_medicos', (err, rows) => {
+        if (err) {
             req.app.get("errManager")(res, err.message, "Failed to get insumos medicos.");
-        }else{
+        } else {
             res.status(200).json(rows);
         }
     })
 });
 
-app.get("/:id", (req, res) =>{
-    req.app.get("db").query('SELECT * FROM insumos_medicos WHERE id=' + req.params.id, (err, rows) =>{
-        if(err){
+app.get("/:id", (req, res) => {
+    req.app.get("db").query('SELECT * FROM insumos_medicos WHERE id=' + req.params.id, (err, rows) => {
+        if (err) {
             req.app.get("errManager")(res, err.message, "Failed to get insumos_medicos.");
-        }else{
+        } else {
             res.status(200).json(rows[0]);
         }
     })
 })
 
-app.delete("/:id", (req, res) =>{
-    req.app.get("db").query('DELETE FROM insumos_medicos WHERE id=' + req.params.id, (err, rows) =>{
-        if(err){
+app.put("/", (req, res) => {
+    let body = req.body;
+    req.app.get("db").query(`UPDATE insumos_medicos SET nombre='${body.nombre}', cantidad=${body.cantidad}, precio=${body.precio}, descripcion='${body.descripcion}', tipo='${body.tipo}' WHERE id = '${body.id}'`, (err, rows) => {
+        if (err) {
+            req.app.get("errManager")(res, err.message, "Failed to update insumo.");
+        } else {
+            if (rows.affectedRows == 1) {
+                res.status(200).json({ msg: "Insumo editado con exito" });
+            } else {
+                res.status(400).json({ msg: "Error al editar el insumo" });
+            }
+        }
+    })
+})
+
+app.delete("/:id", (req, res) => {
+    req.app.get("db").query('DELETE FROM insumos_medicos WHERE id=' + req.params.id, (err, rows) => {
+        if (err) {
             req.app.get("errManager")(res, err.message, "Failed to delete insumo.");
-        }else{
-            if(rows.affectedRows == 0){
-                res.status(400).json({msg:"Insumo a eliminar no encontrado"});
-            }else{
-                res.status(200).json({msg:"Insumo eliminado con exito"})
+        } else {
+            if (rows.affectedRows == 0) {
+                res.status(400).json({ msg: "Insumo a eliminar no encontrado" });
+            } else {
+                res.status(200).json({ msg: "Insumo eliminado con exito" })
             }
         }
     })
